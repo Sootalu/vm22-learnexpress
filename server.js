2b12b3d39ecf25e9e6803de5f116f1571df42540
Helina Sootalu <Helina.Sootalu@tptlive.ee>
2024-01-17 14:49:39 +0200

    meow

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log ('hello Tree')
+const express = require ('express');
+const app = express();
+const port = 3000;
+
+app.get('/',(req, res)=> {
+    res.send('Hello world!');
+});
+
+app.listen(port, ()=>{
+    console.log(`Server listening at http://localhost:${port}`);
+});
+
